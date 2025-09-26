e29d2052166244a8c5b480e620cf0e923752f88e
Pedro Lima <pedro@brainyit.com.br>
2025-09-25 21:45:05 -0300

    adicionando estilo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Disciplina from './Disciplina';
+function App() {
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <h2>Olá react!</h2>
+      <Disciplina/>
+    </>
+  );
+}
+
+export default App
---
+++ src/Disciplina.tsx
@@ -0,0 +1,10 @@
+import "./Disciplina.css"
+
+export default function Disciplina() {
+    return (
+        <div className="disciplina">
+            <h1>DCC207</h1>
+            <h2>Desenvolvimento Web Front End</h2>
+        </div>
+    )
+}
